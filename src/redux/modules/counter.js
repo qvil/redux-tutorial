46f878b5c7311775e0b96628dbeebd0eff97767b
tshyeon <qvil1127@gmail.com>
2017-07-04 16:09:59 +0900

    [STEP 02] Add Actions, Reducer using ducks pattern

---
+++ src/redux/modules/counter.js
@@ -0,0 +1,31 @@
+// Actions
+export const INCREMENT = 'redux-tutorial/counter/INCREMENT';
+export const DECREMENT = 'redux-tutorial/counter/DECREMENT';
+
+export const increase = () => ({
+    type: INCREMENT
+});
+
+export const decrease = () => ({
+    type: DECREMENT
+});
+
+// Reducer
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }            
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }            
+        default:
+            return state;
+    }
+}
+
+export default reducer;
